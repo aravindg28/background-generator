52fed5bbeffc918fe77ebd855f2814097747908a
aravindg28 <aravindg@outlook.com>
2019-02-16 16:01:29 -0400

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var color1 = document.getElementById("first-color");
+var color2 = document.getElementById("second-color");
+var body = document.querySelector("body");
+var div = document.querySelector("p");
+
+function setGradientColors(){
+  body.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
+  div.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input",setGradientColors);
+color2.addEventListener("input",setGradientColors);
+
