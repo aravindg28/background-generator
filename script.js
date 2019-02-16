bccf54d5173e77e71351975f4e432a01d4de85d5
aravindg28 <aravindg@outlook.com>
2019-02-16 16:17:55 -0400

    enhancements for the first page load
    background gradient matches the color load on the first load, linear gradient value is added for the first load

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var color2 = document.getElementById("second-color");
 var body = document.querySelector("body");
 var div = document.querySelector("p");
 
+setGradientColors();
+
 function setGradientColors(){
   body.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
   div.textContent = body.style.background + ";";
